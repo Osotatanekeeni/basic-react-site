ad1bcd2cc6874aad7661f49d566031636af4d313
Osotatanekeeni <36969775+Osotatanekeeni@users.noreply.github.com>
2022-12-21 18:25:41 +0200

    Created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../Images/Logo';
+import '../Styles/Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <Logo />
+      </div>
+      <div className='nav-items'>
+        <ul>
+            <li className='link'><a href='home'>Home</a></li>
+            <li className='link'><a href='contact'>Contact</a></li>
+            <li className='link'><a href='about'>About</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
