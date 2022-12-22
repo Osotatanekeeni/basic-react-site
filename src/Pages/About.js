1d51e0cfac381a8491ce2cb1ecc130c997d6cfb8
Osotatanekeeni <36969775+Osotatanekeeni@users.noreply.github.com>
2022-12-22 16:37:28 +0200

    Created about page

---
+++ src/Pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import '../Styles/About.css';
+import AboutImage from '../Images/about.svg';
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <div className='landing-content'>
+        <div className='landing-text'>
+          <h2 className='landing-heading'>About Tats' Hub</h2>
+          <div className='about-details'>
+            <p>Insert some meaningful text in here</p>
+          </div>
+        </div>
+        <div className='landing-image'>
+          <img src={AboutImage} alt="Contact" width={600} />
+        </div>
+      </div>
+    </div>
+  )
+}
